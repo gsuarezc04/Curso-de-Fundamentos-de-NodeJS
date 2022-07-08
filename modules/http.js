8c2a3c2589f8f3a01acfadfc143780f02c10d63d
Gabriela Suárez <gsuarezc@unal.edu.co>
2022-07-08 17:24:49 -0500

    modules, https

---
+++ modules/http.js
@@ -0,0 +1,28 @@
+const http = require("http");
+
+const router = (req, res) => {
+  console.log("Nueva petición!");
+  console.log(req.url);
+
+  switch (req.url) {
+    case "/hola":
+      res.write("Hola que tal");
+      res.end();
+      break;
+    default:
+      res.write("Error 404");
+      res.end();
+      break;
+  }
+
+  // res.writeHead(201, { "Content-Type": "text/plain" });
+
+  // // Escribir respuesta al usuario
+  // res.write("Hola, ya se usar HTTP de NodeJS");
+
+  // res.end();
+};
+
+http.createServer(router).listen(3000);
+
+console.log("Escuchando http en el puerto 3000");
